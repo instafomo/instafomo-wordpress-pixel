8d9ad0b77beeed7613207dde474c8e70700daaf3
Instafomo <developers@instafomo.com>
2024-07-29 00:09:33 +0530

    updated code

--- src/instafomo-wp-pixel/includes/settings.php
+++ src/instafomo-wp-pixel/includes/settings.php
@@ -31,15 +31,6 @@ function instafomo_api_key_render() {
     ?>
     <input type='text' name='instafomo_api_key' value='<?php echo esc_attr($options); ?>' required>
     <p>You can find your API key by clicking <a href="https://instafomo.com/login?redirect=account-api" target="_blank">here</a>.</p>
-    <p><strong>About Instafomo:</strong></p>
-    <p>Instafomo is a powerful tool that helps you increase conversions by displaying real-time social proof notifications on your website.</p>
-    <p><strong>Key Benefits:</strong></p>
-    <ul>
-        <li>Boosts trust and credibility</li>
-        <li>Increases conversion rates</li>
-        <li>Easy to set up and use</li>
-        <li>Customizable notifications</li>
-    </ul>
     <?php
 }
 
